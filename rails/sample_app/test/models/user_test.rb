3667813d724f2aced242d30efb81e97260319221
nndd91 <nndd91@hotmail.com>
2017-05-16 21:23:56 +0800

    Added model

---
+++ rails/sample_app/test/models/user_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  def setup
+    @user = User.new(name: "Example user", email: "user@eample.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "      "
+    assert_not @user.valid?
+  end  
+
+end
