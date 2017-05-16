ac7b87ecc54064810d58d9f6175e96ef7dd47789
nndd91 <nndd91@hotmail.com>
2017-05-16 23:36:09 +0800

    Signup

--- rails/sample_app/app/controllers/users_controller.rb
+++ rails/sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+    #yaml = @user.attributes.to_yaml
+    #puts yaml
+    #debugger
+
+  end
+
+  def index
+  end
+
+
 end
