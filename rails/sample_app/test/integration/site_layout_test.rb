663cc9167903d353548eff61d0eb8914043b8721
nndd91 <nndd91@hotmail.com>
2017-05-16 20:28:28 +0800

    layout test?

--- rails/sample_app/test/integration/site_layout_test.rb
+++ rails/sample_app/test/integration/site_layout_test.rb
@@ -3,14 +3,21 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
+    #Going to root Page
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", helf_path
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", contact_path
+
+    #Going Contact Page
     get contact_path
     assert_select "title", full_title('Contact')
+
+    #Going Signup Page
+    get signup_path
+    assert_select "title", full_title('Sign up')
   end
 end
