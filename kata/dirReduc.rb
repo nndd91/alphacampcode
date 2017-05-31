6f4b268fd37b007164a61fbbb11a4b773f6a7bcd
nndd91 <nndd91@hotmail.com>
2017-06-01 00:51:46 +0800

    Refactor dirReduc

--- kata/dirReduc.rb
+++ kata/dirReduc.rb
@@ -1,31 +1,25 @@
 def dirReduc(arr)
-
-  dir_hash = {"NORTH" => "SOUTH",
-                    "SOUTH" => "NORTH",
-                    "EAST" => "WEST",
-                    "WEST" => "EAST"}
+  dir_opposites = { 'NORTH' => 'SOUTH',
+                    'SOUTH' => 'NORTH',
+                    'EAST' => 'WEST',
+                    'WEST' => 'EAST' }
   i = 0
-  j = 0
-  cleared = 0
-  while cleared == 0
-    arr.each_with_index do |direction, i|
-      arr.each_with_index do |direction2, j|
-        if direction2 == dir_hash[direction]
-          puts "Found Pair at #{i} and #{j}"
-          p arr
-          arr.delete_at(j)
-          p arr
-          arr.delete_at(i)
-          p arr
-          break
-        end
-      end
+
+  while i < arr.length
+    if dir_opposites[arr[i]] == arr[i+1]
+      arr.delete_at(i+1)
+      arr.delete_at(i)
+      i = 0
+    else
+      i += 1
     end
-    cleared = 1
   end
+  arr
 end
 
 a = ["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]
 p dirReduc(a)#, ["WEST"])
+puts
+puts
 u=["NORTH", "WEST", "SOUTH", "EAST"]
 p dirReduc(u)#, ["NORTH", "WEST", "SOUTH", "EAST"])
