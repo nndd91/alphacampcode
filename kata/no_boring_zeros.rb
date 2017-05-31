3164e19970f7e89cd149bb44a991722aae87280d
nndd91 <nndd91@hotmail.com>
2017-05-31 10:00:28 +0800

    Update no_boring_zeros.rb

--- kata/no_boring_zeros.rb
+++ kata/no_boring_zeros.rb
@@ -1,12 +1,10 @@
 def no_boring_zeros(num)
   # Place code - get rid of zeros - receive bacon
   num = num.to_s
-  i = num.length - 1
   zero_index = num.length - 1
 
-  while num[i] == '0'
+  while num[zero_index] == '0'
     zero_index -= 1
-    i -= 1
   end
   num.slice(0, zero_index + 1).to_i
 end
