15064d9ed3b749c15bf1e2f47e94751cb8f8c68c
nndd91 <nndd91@hotmail.com>
2017-06-01 22:31:32 +0800

    Kata accum

---
+++ kata/accum.rb
@@ -0,0 +1,15 @@
+def accum(s)
+  array = s.split('')
+  array.each_with_index do |each_letter, index|
+    array[index] = each_letter.upcase + ( each_letter.downcase * index )
+  end.join("-")
+end
+
+
+
+p accum("ZpglnRxqenU")# "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+p accum("NyffsGeyylB")# "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+p accum("MjtkuBovqrU")# "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+p accum("EvidjUnokmM")# "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+p accum("HbideVbxncC")# "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+
